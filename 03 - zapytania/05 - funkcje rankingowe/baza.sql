bd82e1c78589d77262d7a49cad146ec5cd922e68
Piotr Miernik <piotrmiernik255@gmail.com>
2023-06-25 14:05:02 +0200

    Created rank functions queries

---
+++ 03 - zapytania/05 - funkcje rankingowe/baza.sql
@@ -0,0 +1,80 @@
+-- Ranga pracowników w kwiaciarniach na podstawie ilości sprzedanych kwiatów:
+WITH
+  RankedSales
+  AS
+  (
+    SELECT
+      dp.imie AS imie_pracownika,
+      dp.nazwisko AS nazwisko_pracownika,
+      k.nazwa AS nazwa_kwiaciarni,
+      SUM(pp.ilosc) AS ilosc_sprzedanych_kwiatow,
+      RANK() OVER (PARTITION BY r.id_kwiaciarni ORDER BY SUM(pp.ilosc) DESC) AS ranga
+    FROM
+      pozycja_paragonu pp
+      JOIN rachunki r ON pp.id_rachunku = r.id_rachunku
+      JOIN pracownicy pr ON r.id_pracownika = pr.id_pracownika
+      JOIN dane_personalne dp ON pr.id_danych = dp.id_danych
+      JOIN kwiaciarnie k ON r.id_kwiaciarni = k.id_kwiaciarni
+    WHERE pp.id_gatunku IS NOT NULL
+    GROUP BY
+        r.id_kwiaciarni, dp.imie, dp.nazwisko, k.nazwa
+  )
+SELECT
+  imie_pracownika,
+  nazwisko_pracownika,
+  nazwa_kwiaciarni,
+  ilosc_sprzedanych_kwiatow,
+  ranga
+FROM
+  RankedSales;
+-- Sens praktyczny: Pozwala na porównanie wyników sprzedaży pracowników w ramach danej kwiaciarni, co może być wykorzystane do motywowania personelu.
+
+-- Przydzielanie klientom poziomów lojalnościowych na podstawie wydanej kwoty:
+SELECT
+  dp.imie AS imie_klienta,
+  dp.nazwisko AS nazwisko_klienta,
+  SUM(g.cena * pp.ilosc) AS calkowita_kwota,
+  NTILE(3) OVER (ORDER BY SUM(g.cena * pp.ilosc) DESC) AS poziom_lojalnosci
+FROM
+  pozycja_paragonu pp
+  JOIN rachunki r ON pp.id_rachunku = r.id_rachunku
+  JOIN klienci kl ON r.id_klienta = kl.id_klienta
+  JOIN dane_personalne dp ON kl.id_danych = dp.id_danych
+  JOIN gatunki g ON pp.id_gatunku = g.id_gatunku
+WHERE pp.id_gatunku IS NOT NULL
+GROUP BY
+    kl.id_klienta, dp.imie, dp.nazwisko;
+-- Sens praktyczny: Pozwala na segmentację klientów według wydanej kwoty, co może być użyte do wprowadzenia programów lojalnościowych.
+
+-- Znalezienie 3 najpopularniejszych gatunków kwiatów w każdej kwiaciarni:
+WITH
+  RankedFlowers
+  AS
+  (
+    SELECT
+      k.nazwa AS nazwa_kwiaciarni,
+      g.nazwa AS gatunek_kwiatu,
+      SUM(pp.ilosc) AS ilosc_sprzedanych,
+      RANK() OVER (PARTITION BY r.id_kwiaciarni ORDER BY SUM(pp.ilosc) DESC) AS ranga
+    FROM
+      pozycja_paragonu pp
+      JOIN rachunki r ON pp.id_rachunku = r.id_rachunku
+      JOIN gatunki g ON pp.id_gatunku = g.id_gatunku
+      JOIN kwiaciarnie k ON r.id_kwiaciarni = k.id_kwiaciarni
+    WHERE pp.id_gatunku IS NOT NULL
+    GROUP BY
+      r.id_kwiaciarni, g.nazwa, k.nazwa
+  )
+SELECT
+  nazwa_kwiaciarni,
+  gatunek_kwiatu,
+  ilosc_sprzedanych,
+  ranga
+FROM
+  RankedFlowers
+WHERE
+      ranga <= 3
+ORDER BY
+      nazwa_kwiaciarni,
+      ranga;
+-- Sens praktyczny: Pozwala na zrozumienie, jakie gatunki kwiatów cieszą się największą popularnością w poszczególnych kwiaciarniach, co może wpłynąć na decyzje zakupowe.
--- 03 - zapytania/05 - funkcje rankingowe/hurtownia.sql
+++ 03 - zapytania/05 - funkcje rankingowe/hurtownia.sql
@@ -1,13 +1,29 @@
 -- Ranga pracowników w kwiaciarniach na podstawie ilości sprzedanych kwiatów:
-SELECT dp.imie AS imie_pracownika,
-  dp.nazwisko AS nazwisko_pracownika,
-  dk.nazwa AS nazwa_kwiaciarni,
-  SUM(fs.ilosc) AS ilosc_sprzedanych_kwiatow,
-  RANK() OVER (PARTITION BY fs.id_kwiaciarni ORDER BY SUM(fs.ilosc) DESC) AS ranga
-FROM Fakt_Sprzedazy fs
-  JOIN Dim_Pracownik dp ON fs.id_pracownika = dp.id_pracownika
-  JOIN Dim_Kwiaciarnia dk ON fs.id_kwiaciarni = dk.id_kwiaciarni
-GROUP BY fs.id_pracownika, dp.imie, dp.nazwisko, dk.nazwa;
+WITH
+  RankedSales
+  AS
+  (
+    SELECT
+      dp.imie AS imie_pracownika,
+      dp.nazwisko AS nazwisko_pracownika,
+      dk.nazwa AS nazwa_kwiaciarni,
+      SUM(fs.ilosc) AS ilosc_sprzedanych_kwiatow,
+      RANK() OVER (PARTITION BY fs.id_kwiaciarni ORDER BY SUM(fs.ilosc) DESC) AS ranga
+    FROM
+      Fakt_Sprzedazy fs
+      JOIN Dim_Pracownik dp ON fs.id_pracownika = dp.id_pracownika
+      JOIN Dim_Kwiaciarnia dk ON fs.id_kwiaciarni = dk.id_kwiaciarni
+    GROUP BY
+        fs.id_kwiaciarni, dp.imie, dp.nazwisko, dk.nazwa
+  )
+SELECT
+  imie_pracownika,
+  nazwisko_pracownika,
+  nazwa_kwiaciarni,
+  ilosc_sprzedanych_kwiatow,
+  ranga
+FROM
+  RankedSales;
 -- Sens praktyczny: Pozwala na porównanie wyników sprzedaży pracowników w ramach danej kwiaciarni, co może być wykorzystane do motywowania personelu.
 
 -- Przydzielanie klientom poziomów lojalnościowych na podstawie wydanej kwoty:
@@ -21,13 +37,29 @@ GROUP BY fs.id_klienta, dk.imie, dk.nazwisko;
 -- Sens praktyczny: Pozwala na segmentację klientów według wydanej kwoty, co może być użyte do wprowadzenia programów lojalnościowych.
 
 -- Znalezienie 3 najpopularniejszych gatunków kwiatów w każdej kwiaciarni:
-SELECT dk.nazwa AS nazwa_kwiaciarni,
-  dg.nazwa AS gatunek_kwiatu,
-  SUM(fs.ilosc) AS ilosc_sprzedanych,
-  RANK() OVER (PARTITION BY fs.id_kwiaciarni ORDER BY SUM(fs.ilosc) DESC) AS ranga
-FROM Fakt_Sprzedazy fs
-  JOIN Dim_Gatunek dg ON fs.id_gatunku = dg.id_gatunku
-  JOIN Dim_Kwiaciarnia dk ON fs.id_kwiaciarni = dk.id_kwiaciarni
-GROUP BY fs.id_kwiaciarni, dg.nazwa, dk.nazwa
-HAVING RANK() OVER (PARTITION BY fs.id_kwiaciarni ORDER BY SUM(fs.ilosc) DESC) <= 3;
+WITH
+  RankedFlowers
+  AS
+  (
+    SELECT
+      dk.nazwa AS nazwa_kwiaciarni,
+      dg.nazwa AS gatunek_kwiatu,
+      SUM(fs.ilosc) AS ilosc_sprzedanych,
+      RANK() OVER (PARTITION BY fs.id_kwiaciarni ORDER BY SUM(fs.ilosc) DESC) AS ranga
+    FROM
+      Fakt_Sprzedazy fs
+      JOIN Dim_Gatunek dg ON fs.id_gatunku = dg.id_gatunku
+      JOIN Dim_Kwiaciarnia dk ON fs.id_kwiaciarni = dk.id_kwiaciarni
+    GROUP BY
+        fs.id_kwiaciarni, dg.nazwa, dk.nazwa
+  )
+SELECT
+  nazwa_kwiaciarni,
+  gatunek_kwiatu,
+  ilosc_sprzedanych,
+  ranga
+FROM
+  RankedFlowers
+WHERE
+    ranga <= 3;
 -- Sens praktyczny: Pozwala na zrozumienie, jakie gatunki kwiatów cieszą się największą popularnością w poszczególnych kwiaciarniach, co może wpłynąć na decyzje zakupowe.
